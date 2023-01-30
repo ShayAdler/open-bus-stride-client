5d1852f2d0e10a2f5eb407dc1b291ed6e57c9332
Shay <shay@bionicstork.com>
2023-01-30 19:50:27 +0200

    Added csv route

--- scripts/planned_vs_actual_comparison/app.py
+++ scripts/planned_vs_actual_comparison/app.py
@@ -1,5 +1,10 @@
-from flask import Flask
-from get_missing_rides import find_gtfs_and_siri_for_date
+import csv
+
+from flask import Flask, Response, make_response
+from get_missing_rides import find_gtfs_and_siri_for_date, parse_date_str, RideStatisticsOutput
+from io import StringIO
+from datetime import timedelta
+
 
 app = Flask(__name__)
 
@@ -8,11 +13,42 @@ app = Flask(__name__)
 def hello():
     return 'Hello, World!'
 
+
 @app.route('/get_rides_statistics/<line_ref>/<operator_ref>/<date>')
 def get_rides_statistics(line_ref=None, operator_ref=None, date=None):
-    return find_gtfs_and_siri_for_date(date, operator_ref, line_ref).to_dict()
+    response = find_gtfs_and_siri_for_date(date, operator_ref, line_ref)
+    if response is None:
+        return {}
+
+    return response.to_dict()
+
+
+@app.route('/get_statistics_csv/<line_refs>/<operator_refs>/<date_from>/<date_to>')
+def get_statistics_csv(line_refs=None, operator_refs=None, date_from=None, date_to=None):
+    line_refs = line_refs.split(",")
+    operator_refs = operator_refs.split(",")
+    if len(line_refs) != len(operator_refs):
+        return Response("line_refs and operator_refs should be corresponding", status=400, mimetype='application/json')
+
+    csv_data = StringIO()
+    writer = csv.writer(csv_data)
+    writer.writerow(RideStatisticsOutput.csv_format())
+    delta = parse_date_str(date_to) - parse_date_str(date_from)
+    for operator_ref, line_ref in zip(line_refs, operator_refs):
+        for i in range(delta.days + 1):
+            day = parse_date_str(date_from) + timedelta(days=i)
+            date_statistics = find_gtfs_and_siri_for_date(day.__str__(), line_ref, operator_ref)
+            if date_statistics is not None:
+                writer.writerow(date_statistics.to_csv_line())
+
+    csv_data.seek(0)
+    response = make_response(csv_data)
+    response.headers["Content-Disposition"] = f"attachment; filename={'_'.join(line_refs)}-{date_from}-{date_to}-statistics.csv"
+    response.headers["Content-type"] = "text/csv"
+    return response
+
 
 if __name__ == '__main__':
-    app.run("0.0.0.0", 80)
+    app.run("0.0.0.0", 5001)
 
 
